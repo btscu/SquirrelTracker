11f09e9b69b3372519b4a80384e2ca71a418ca39
Ben Smith <ben.smith@columbia.edu>
2020-05-02 20:30:39 +0000

    Update URLs, add HTML for Homepage, create templates directory

--- sightings/urls.py
+++ sightings/urls.py
@@ -2,5 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-        path('', views.index),      
+        path('', views.homepage),
+       # path('sightings', views.all_sights),
+       # path('sightings/add/', views.add_sights),
+       # path('sightings/stats/', views.stats),
+       # path('sightings/<Unique_Squirrel_Id>/', views.find_squirrel)
+       # path('map/', views.map)
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SquirrelViewing
+from .modelform import ViewForm
 
 
-def index(request):
-    return HttpResponse('Squirrel Sightings')
+# def index(request):
+#    return HttpResponse('Squirrel Sightings')
+
+
+def homepage(request):
+    return render(request,'templates/home.html')
+
+
+
+
+
+
+
 
 # Create your views here.
