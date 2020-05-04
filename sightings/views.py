fbb4452aa53b9e00dc0b19714ee316827c7159eb
Ben Smith <ben.smith@columbia.edu>
2020-05-04 01:49:46 +0000

    Finish specific squirrel view and integrate with list view

--- sightings/views.py
+++ sightings/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import SquirrelViewing
 from .modelform import ViewForm
 
+
 # def index(request):
 #    return HttpResponse('Squirrel Sightings')
 
@@ -25,7 +26,12 @@ def squirrel_list(request):
 
 def specific_squirrel(request, Unique_Squirrel_Id):
     specific_squirrel  = SquirrelViewing.objects.get(unique_squirrel_id = Unique_Squirrel_Id)
-    return render(request, 'templates/specific_squirrel.html')
+    info = ['Unique_Squirrel_Id', 'Longitude', 'Latitude', 'Date', 'Shift', 'Age']
+    form = ViewForm(instance = specific_squirrel)
+    context = {
+            'squirrels':specific_squirrel
+            }
+    return render(request, 'templates/specific_squirrel.html', context)
 
 
 
