deb645b86d341dc99c2e6f1f222664819394f52e
Dhruv Gondalia <dag2221@columbia.edu>
2020-05-09 14:21:19 +0000

    Clearing unnecessary code

--- sightings/views.py
+++ sightings/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponse
 from .models import SquirrelViewing
 from .modelform import ViewForm
 
-
-# def index(request):
-#    return HttpResponse('Squirrel Sightings')
-
-
 def homepage(request):
     return render(request,'templates/home.html')
 
