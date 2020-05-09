a9dd31b2e9ec24803c0a8dec166af75904222d3c
Dhruv Gondalia <dag2221@columbia.edu>
2020-05-09 15:30:51 +0000

    Removing the add squirrel url

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 urlpatterns = [
         path('', views.homepage),
         path('sightings/', views.squirrel_list),
-       # path('sightings/add/', views.add_sights),
         path('sightings/stats/', views.stats),
         path('sightings/<Unique_Squirrel_Id>/', views.specific_squirrel), 
         path('map/', views.squirrel_map)
